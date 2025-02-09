df0a0a06b3667806b0fadb648344a28fedbe21bf
andrewbelles <andrewbelles31@gmail.com>
2025-02-09 17:37:28 -0500

    Rewrite of program

--- outlier.asm
+++ outlier.asm
@@ -1,7 +1,6 @@
 section .note.GNU-stack noalloc noexec nowrite progbits
 
-section .rodata
-
+section .rodata 
   print_iter db  "Iter    : %s",  6, 0
   print_sigma db "Sigma   : %s", 10, 0
   print_mean db  "Mean    : %s", 10, 0
@@ -11,9 +10,9 @@ section .rodata
   print_test db "Test", 0
   print_newl db " ", 0
 
-  ; test for andpd for absolute value 
-  align 16
-  mask dq 0x7FFFFFFFFFFFFFF, 0x7FFFFFFFFFFFFFF
+  ; test for andpd for absolute value
+  align 16 
+  mask dq 0x7FFFFFFFFFFFFFFF, 0 
   
   format_float db "%f", 0  
   format_int   db "%d", 0
@@ -21,25 +20,19 @@ section .rodata
 
 section .text
   extern sprintf, printf, strtod
-  global main 
-  
-; Program follows libc convention for commandline arguments
-;   [argc] [argv[0]] [argv[1]] ... 
-;   rdi <= argc 
-;   rsi <= *argv[]
+  global main
 
 main:
 
-  ; check argument count 
+  ; argc check 
   cmp rdi, 4
-  jl error 
+  jl error
 
-  mov rbx, [rsi + 8]              ; argv[1] input name 
-  mov r13, [rsi + 16]             ; argv[2] output name
-  mov rbp, [rsi + 24]             ; argv[3] (threshold) is 16 bytes offset
+  mov rbx, [rsi + 8]
+  mov r13, [rsi + 16]
+  mov rbp, [rsi + 24]
 
-  ; 512 bytes for aggregate values, scanf 
-  mov rax, 9 
+  mov rax, 9
   xor rdi, rdi
   mov rsi, 512                 
   mov rdx, 3 
@@ -52,27 +45,269 @@ main:
   test rax, rax
   js error
 
-  mov r12, rax                    ; store address of heap for aggregates at r12
+  mov r12, rax
 
-  ; call c library string to double to process threshold (will be placed in r12 + 8 (after fd))
+  sub rsp, 8
+
+  ; process threshold 
   lea rdi, [rbp]
-  xor rsi, rsi
+  xor rsi, rsi 
   call strtod
 
-  movsd [r12 + 16], xmm0          ; store in low memory on heap 
+  ; threshold
+  movsd [r12 + 16], xmm0
 
-  ; open read file 
-  mov rax, 2                      ; sys_open
+  ; read file 
+  mov rax, 2
   mov rdi, rbx
-  xor rsi, rsi                    ; read mode  
+  xor rsi, rsi 
+  syscall 
+
+  ; check output 
+  cmp rax, 0
+  jl error
+
+  ; place write fd
+  mov [r12], rax 
+
+  pxor xmm0, xmm0
+  movsd [r12 + 48], xmm0              ; mean
+  movsd [r12 + 64], xmm0              ; M2
+
+  xor rcx, rcx
+
+.loop:
+
+  push rcx
+
+  ; prepare read syscall
+  mov rax, 0
+  mov rdi, [r12]
+  lea rsi, [r12 + 32]
+  mov rdx, 8
+  syscall 
+  
+  pop rcx
+
+  cmp rax, 8
+  je .sigmaCall
+
+  cmp rax, 0 
+  je .eof 
+
+sigmaCall:
+  
+  ; aggregate m2 and mean
+  mov rdi, rcx
+  
+  push rcx 
+
+  mov rsi, [r12 + 48]
+  mov rdx, [r12 + 64]
+  movsd xmm0, [r12 + 32]
+  call _sigma
+
+  pop rcx
+
+  jmp .loop
+
+eof: 
+  
+  ; calc standard deviation 
+  movsd xmm0, [r12 + 64]
+  mov rax, rcx
+  dec rax 
+  cvtsi2sd xmm1, rax 
+  divsd xmm0, xmm1 
+  sqrtsd xmm0, xmm0 
+  movsd [r12 + 64], xmm0
+
+  ; take count and create new memory for clean loop
+  ; rcx holds count 
+  mov [r12 + 80], rcx
+  imul rcx, 3
+  imul rcx, 8
+
+  ; syscall for array memory
+  mov rax, 9
+  xor rdi, rdi 
+  mov rsi, rcx
+  mov rdx, 3
+  mov r10, 34 
+  mov r8, -1
+  xor r9, r9
   syscall
 
-  ; check return value of open
-  cmp rax, 0
-  jl error 
-  mov [r12], rax                  ; file descriptor first value in r12 
+  test rax, rax 
+  js error
 
-  ; open write file 
+  ; allocate full, clean, and outlier arrays
+  mov r13, rax
+  mov rcx, [r12 + 80]
+  imul rcx, 8
+  add rax, rcx 
+  mov r14, rax
+  add rax, rcx
+  mov r15, rcx
+
+  ; reset file pointer location
+  mov rax, 8
+  mov rdi, [r12]
+  xor rsi, rsi
+  xor rdx, rdx
+  syscall
+
+  xor r8, r8
+
+  ; mean in r12 + 144
+  ; m2   in r12 + 160
+outlierLoop:
+
+  cmp r8, 0 
+  jne .checkEnd
+
+  ; if first iteration calculation first threshold 
+  push r8
+
+  mov rdi, [r12 + 16]
+  mov rsi, [r12 + 64]
+  call _threshold
+
+  movsd [r12 + 128], xmm0
+
+  pop r8
+
+  jmp .read
+
+  .checkEnd:
+
+  ;  check current outlier count with previous count 
+  cmp r10, [r12 + 96]
+  je end 
+
+  ; reset counters used 
+
+  xor rcx, rcx 
+  xor r8, r8
+  xor r9, r9
+  xor r10, r10
+  xor r11, r11
+
+  ; move clean count into current count 
+  mov [r12 + 112], r9
+  mov [r12 + 172], r10              ; keep track of n outliers
+
+  ; move clean array into full array 
+  mov r13, r14
+
+  ; calc stdev and move mean 
+  movsd xmm0, [r12 + 160]
+  dec r9
+  cvtsi2sd xmm1, r9
+  divsd xmm0, xmm1 
+  sqrtsd xmm0, xmm0
+  movsd [r12 + 64], xmm0 
+  movsd xmm0, [r12 + 144]
+  movsd [r12 + 48], xmm0
+
+  push r8
+
+  ; update threshold 
+  mov rdi, [r12 + 16]
+  mov rsi, [r12 + 64]
+  call _threshold
+  
+  pop r8
+
+  .read:
+  
+  push rcx
+  push r8
+  push r9 
+  push r10
+  push r11
+
+  sub rsp, 8
+  
+  mov rax, 0 
+  mov rdi, [r12]
+  lea rsi, [r12 + 32]
+  mov rdx, 8
+  syscall
+
+  add rsp, 8
+  
+  pop r11
+  pop r10 
+  pop r9 
+  pop r8
+  pop rcx
+
+  mov rsi, 8
+  imul rsi, rcx 
+  movsd xmm0, [r12 + 32]
+  movsd [r13 + rsi], xmm0
+
+  inc rcx
+
+  cmp rcx, [r12 + 80]
+  jne .read
+
+  ; clear count accumulated from array indexing
+  xor rcx, rcx 
+  je .clean
+
+  .clean:
+
+  mov rsi, 8 
+  imul rsi, r11
+  movsd xmm0, [r13 + rsi]
+
+  inc r11 
+
+  ; check if abs(residual) < sigma * threshold
+  subsd xmm0, [r12 + 48]
+  movapd xmm1, [mask]
+  andpd xmm0, xmm1
+  movsd xmm1, [r12 + 128]
+  ucomisd xmm0, xmm1 
+  jg .outlier 
+
+  mov rbp, 8
+  imul rbp, r9 
+  inc r9
+  movsd xmm0, [r13 + rsi]
+  movsd [r14 + rbp], xmm0
+
+  ; call sigma calculation
+  mov rsi, [r12 + 48]
+  mov rdx, [r12 + 64]
+  movsd xmm0, [r14 + rbp] 
+  call _sigma
+
+  cmp r11, [r12 + 112]
+  je .checkEnd
+
+  jmp .clean
+
+  .outlier:
+  
+  mov rbp, 8
+  imul rbp, r10
+  inc r10
+  movsd xmm0, [r13 + rsi]
+  movsd [r15 + rbp], xmm0 
+
+  cmp r11, [r12 + 112]
+  je .checkEnd
+
+  jmp .clean
+
+end: 
+  
+  ; print all outliers to file
+  
+  ; sysopen read file
   mov rax, 2 
   mov rdi, r13 
   mov rsi, 01h | 40h | 200h
@@ -81,430 +316,83 @@ main:
 
   cmp rax, 0 
   jl error 
-  mov [r12 + 256], rax            ; write file descriptor
+  mov [r12 + 8], rax
 
-  ; full file mean, M2, count, sigma calc
+  ; should prob free memory here but oops
+  xor r13, r13 
+  .printloop:
 
-  pxor xmm0, xmm0
-  movsd [r12 + 48], xmm0          ; mean = 0.0
-  movsd [r12 + 48], xmm0          ; M2   = 0.0
-  xor rcx, rcx
+  xor rdx, rdx
+  mov rax, rsp
+  mov rcx, 16
+  div rcx 
+  cmp rdx, 0 
+  je .aligned 
 
-  sub rsp, 8
+  mov rbx, rdx 
+  sub rsp, rbx 
 
-fileLoop:
+  .aligned: 
 
-  push rcx                        ; reserve rcx through syscall
-
-  ; read a single double value   
-  mov rax, 0                      ; sys_read
-  mov rdi, [r12]                  ; file descriptor
-  lea rsi, [r12 + 32]             ; value buffer
-  mov rdx, 8                      ; 8 bytes to read 
-  syscall
-
-  pop rcx
-
-  cmp rax, 8
-  je calcSigma 
-
-  cmp rax, 0 
-  je eofHandle
-
-calcSigma:
-
-  ; find standard deviation using running total
-  inc rcx                         ; count++
-
-  ; set value and curr mean
-  movsd xmm0, [r12 + 32]
-  movsd xmm1, [r12 + 48]
-  
-  subsd xmm0, xmm1                ; delta = value - mean
-  movsd [r12 + 172], xmm0         ; store delta in shared/temporary heap location 
-
-  cmp rcx, 0 
-  je error
-
-  ; get running mean from delta / count 
-  cvtsi2sd xmm1, rcx             
-  divsd xmm0, xmm1
-  addsd xmm0, [r12 + 48]
-  movsd [r12 + 48], xmm0          ; mean += delta / count  
-
-  movsd xmm0, [r12 + 32]
-  subsd xmm0, [r12 + 48]          ; delta2 = value - mean(new)
-
-  mulsd xmm0, [r12 + 172]         ; delta * delta2 
-  addsd xmm0, [r12 + 64]
-  movsd [r12 + 64], xmm0          ; add to M2 
-
-  jmp fileLoop
-
-eofHandle: 
-
-  mov [r12 + 80], rcx             ; store count
-
-  ; pull M2 from heap
-  movsd xmm0, [r12 + 64]
-  mov rax, rcx
-  dec rax
-  cvtsi2sd xmm1, rax
-
-  ; sigma = sqrt(M2 / count - 1)
-  divsd xmm0, xmm1
-  sqrtsd xmm0, xmm0
-  movsd [r12 + 64], xmm0          ; store sigma
-
-  ; use standard dev in xmm0 and place type to format string from and call 
-  lea rdi, [r12 + 172]
-  lea rsi, [format_float] 
-  mov rax, 1
-  call sprintf
-
-  ; set format and variable to print and call
-  lea rdi, [print_sigma]
-  lea rsi, [r12 + 172]
-  mov rax, 0
-  call printf
-
-  ; load mean into string var and print 
-  movsd xmm0, [r12 + 48]
-  lea rdi, [r12 + 172]
-  lea rsi, [format_float] 
-  mov rax, 1
-  call sprintf
-
-  lea rdi, [print_mean]
-  lea rsi, [r12 + 172]
-  mov rax, 0
-  call printf
-  
-  ; load count into string var and print
-  mov rdx, [r12 + 80]
-  lea rdi, [r12 + 172]
-  lea rsi, [format_int] 
-  xor rax, rax
-  call sprintf
-
-  ; set format and variable to print and call
-  lea rdi, [print_coun]
-  lea rsi, [r12 + 172]
-  mov rax, 0
-  call printf
-
-  lea rdi, [format_str]
-  lea rsi, [print_newl] 
-  mov rax, 0 
-  call printf
-
-  mov rbx, [r12 + 80]
-  imul rbx, 3
-  imul rbx, 8                      ; count * 3 arrays * 8 bytes per float
-
-  ; allocate region for arrays  
-  mov rax, 9 
-  xor rdi, rdi
-  mov rsi, rbx
-  mov rdx, 3 
-  mov r10, 34
-  mov r8, -1
-  xor r9, r9
-  syscall
-
-  test rax, rax
-  js error 
-  
-  ; r13 is the full array 
-  ; r14 is the clean array 
-  ; r15 is the outlier array 
-  
-  ; allocating arrays 
-  mov r13, rax                    ; store arary addresses at r13, r14, r15 
-  mov rbx, [r12 + 80]
-  imul rbx, 8
-  add rax, rbx
-  mov r14, rax
-  add rax, rbx
-  mov r15, rax
-
-  ; test print
-  lea rdi, [format_str]
-  lea rsi, [print_test]
-  xor rax, rax 
-  call printf
-
-  mov rbx, [r12 + 80]
-  mov [r12 + 180], rbx            ; store original size in aggregate heap
-  mov [r12 + 196], r13            ; store original address in agg. heap 
-
-  ; reset file pointer location 
-  mov rax, 8                      ; lseek ~= fseek(fptr, 0, SEEK_SET)
-  mov rdi, [r12]                  ; fd
-  xor rsi, rsi
-  xor rdx, rdx 
-  syscall
-
-  ; test print
-  lea rdi, [format_str]
-  lea rsi, [print_test]
-  xor rax, rax 
-  call printf  
-
-  ; check return value of open
-  cmp rax, 0
-  jl error 
-  mov [r12 + 8], rax              ; file descriptor first value in r12 
-  
-  ; test print
-  lea rdi, [format_str]
-  lea rsi, [print_test]
-  xor rax, rax 
-  call printf  
-
-  ; init counters 
-  xor r8, r8                      ; iter counter
-
-checkEnd: 
-
-  ; first iter check
-  cmp r8, 0
-  je outlierLoop
-
-  cmp r10, [r12 + 160]            ; test current outlier count with previous count
-  ; set end flag? 
-  je handleEnd                    ; handle end of program if true 
-
-  mov [r12 + 160], r9              ; move current into previous count if not the end
-
-outlierLoop:
-
-  ; counters reset per iteration
-  xor r11, r11
-  xor rcx, rcx                    ; value counter  
-  xor r9, r9                      ; outlier counter
-  xor r10, r10                    ; clean counter
-
-  ; shift curr aggregate registers to previous aggregate registers and reset car
-
-  ; first iter check
-  cmp r8, 0 
-  je threshCalc 
-
-  ; shift values in heap
-  movsd xmm0, [r12 + 96]
-  movsd [r12 + 64], xmm0          ; 96 holds both M2, and sigma
-  movsd xmm0, [r12 + 144]
-  movsd [r12 + 48], xmm0          ; shift current mean into previous mean 
-
-  ; shift clean array onto current array (r14 -> r13)
-  ; and shift clean count onto real count
-  mov r13, r14
-  mov [r12 + 64], r9 
-
-threshCalc:
-
-  ; calculate sigma*threshold with previous sigma and place in r12 + 128
-  movsd xmm0, [r12 + 16]
-  mulsd xmm0, [r12 + 64]
-  movsd [r12 + 128], xmm0
-
-  cmp r8, 0 
-  je readLoop 
-  
-readLoop: 
-  
-  ; push counters onto stack  
-  push rcx
-  push r8
-  push r9
-  push r10 
-  push r11
-
-  ; read double to agggregate heap
-  mov rax, 0                      ; sys_read
-  mov rdi, [r12]              
-  lea rsi, [r12 + 32]             ; double buffer
-  mov rdx, 8                      ; 8 bytes to read 
-  syscall
-
-  ; revert values 
-  pop rcx 
-  pop r8 
-  pop r9 
-  pop r10
-  pop r11
-
-  ; place value in array r13
-  ; r13[i] = [r13 + rcx * 8]
-  mov rsi, 8
-  imul rsi, rcx
-  movsd xmm0, [r12 + 32]
-  movsd [r13 + rsi], xmm0         ; iterate array r13 
-
-  inc rcx 
-
-  ; check if read the count in file 
-  cmp rcx, [r12 + 80]
-  jne readLoop                     ; loop till eof 
-
-  je arrayLoop 
-
-arrayLoop:
-  ; r8 loops to previous count (stored in rcx)
-
-  ; load value from array. if abs(array - previous aggregate mean) < threshold * previous sigma add to outliers
-  mov rsi, 8
-  imul rsi, r11
-  movsd xmm0, [r13 + rsi]
-
-  inc r11                          ; full counter 
-
-  subsd xmm0, [r12 + 48]
-  movapd xmm1, [mask]
-  andpd xmm0, xmm1                 ; abs(residual)
-
-  movsd xmm1, [r12 + 128]
-  ucomisd xmm0, xmm1 
-  jg outlier
-  
-  ; load into clean arr
+  ; get count 
   mov rbp, 8
-  imul rbp, r9
-  movsd xmm0, [r13 + rsi]
-  movsd [r14 + rbp], xmm0 
-
-  inc r9                           ; iterate clean count 
-
-  jmp calcValues
-
-outlier:
-
-  ; index outlier array and place value within it
-  mov rbp, 8
-  imul rbp, r10
-  movsd xmm0, [r13 + rsi]
-  movsd [r15 + rbp], xmm0 
-
-  inc r10                          ; increment outlier count 
-
-  ; push counters onto stack
-  push rcx
-  push r8
-  push r9
-  push r10
-  push r11 
-
-  ; sprintf call
+  imul rbp, r11
   movsd xmm0, [r15 + rbp]
+  lea rdi, [r12 + 192]
+  lea rsi, [format_float]
+  mov rax, 1 
+  call sprintf 
+  
+  xor rcx, rcx
+
+  .str_len:
+
+  cmp byte [rdi + rcx], 0 
+  je .write 
+  inc rcx 
+  jmp .str_len
+
+  .write: 
+
+  mov rax, 1 
+  mov rdi, [r12 + 8]
+  lea rsi, [r12 + 192]
+  mov rdx, rcx 
+  syscall
+
+  lea rsi, [print_newl]
+  mov rdx, 1 
+  syscall
+
+  inc r13
+
+  cmp r13, [r12 + 172]
+  jl .printloop
+  
+  ; print final found values to user 
+  ; final sigma calc 
+  movsd xmm0, [r12 + 160]
+  mov rcx, [r12 + 112]
+  dec rcx
+  cvtsi2sd xmm1, rcx
+  divsd xmm0, xmm1
+  movsd [r12 + 160], xmm0
+
+  ; print outlier count 
+  mov rdx, [r12 + 112]
   lea rdi, [r12 + 172]
   lea rsi, [format_float] 
   mov rax, 1
   call sprintf
 
-  ; revert registers 
-  pop rcx
-
-  lea rdi, [r12 + 172]
-  
-  mov [r12 + 212], rcx
-
-  xor rcx, rcx
-
-  ; count the number of characters in string to print
-str_len:
-
-  ; compare each character to 0 
-  cmp byte [rdi + rcx], 0
-  je write_out
-  inc rcx
-  jmp str_len                      ; loop if not end
-
-write_out:
-
-  ; sys_write string to file
-  mov rax, 1
-  mov rdi, [r12 + 256]             ; outliers.txt file
-  lea rsi, [r12 + 172]             ; string formatted by sprintf
-  mov rdx, rcx                     ; size of string  
-  
-  push rcx
-
-  syscall
-
-  ; add newline after 
-  lea rsi, [print_newl]
-  mov rdx, 1
-  syscall
-
-  pop rcx
-  pop r8
-  pop r9
-  pop r10
-  pop r11
-
-  jmp arrayLoop 
-
-calcValues: ; new aggregate values are placed in a new region on the aggregate heap
-
-  ; find standard deviation using running total
-
-  ; set value and curr mean
-  movsd xmm0, [r14 + rbp]
-  movsd xmm1, [r12 + 144]
-  
-  subsd xmm0, xmm1                 ; delta = value - mean
-  movsd [r12 + 172], xmm0          ; store delta in shared/temp memory
-
-  cmp r9, 0 
-  je error
-
-  ; get running mean from delta / count 
-  cvtsi2sd xmm1, r9               ; load clean count as a double          
-  divsd xmm0, xmm1
-  addsd xmm0, [r12 + 144]
-  movsd [r12 + 144], xmm0          ; mean += delta / count  
-
-  movsd xmm0, [r14 + rbp]
-  subsd xmm0, [r12 + 144]          ; delta2 = value - mean(new)
-
-  mulsd xmm0, [r12 + 172]          ; delta * delta2 
-  addsd xmm0, [r12 + 96]
-  movsd [r12 + 96], xmm0           ; curr M2  
-
-  cmp r11, [r12 + 80]              ; check if the value counter equals total count
-  je printOutput
-
-  jmp arrayLoop
-
-printOutput:
-
-  ; finished iteration
-  inc r8
-
-  ; pull final M2 and store back in same memory location 
-  movsd xmm0, [r12 + 96]  
-  mov rax, r9
-  dec rax
-  cvtsi2sd xmm1, rax
-  divsd xmm0, xmm1
-  sqrtsd xmm0, xmm0
-  movsd [r12 + 96], xmm0          ; store sigma
-
-  ; formatted data printed here per iteration (including final iter) 
-  push rcx
-  push r8
-  push r9 
-  push r10
-
-; set format and variable to print and call
+  ; set format and variable to print and call
   lea rdi, [print_sigma]
   lea rsi, [r12 + 172]
   mov rax, 0
   call printf
 
   ; load mean into string var and print 
-  movsd xmm0, [r12 + 144]
+  mov rdx, [r12 + 144]
   lea rdi, [r12 + 172]
   lea rsi, [format_float] 
   mov rax, 1
@@ -516,7 +404,7 @@ printOutput:
   call printf
   
   ; load count into string var and print
-  mov rdx, [r12 + 160]
+  movsd xmm0, [r12 + 160]
   lea rdi, [r12 + 172]
   lea rsi, [format_int] 
   xor rax, rax
@@ -528,56 +416,101 @@ printOutput:
   mov rax, 0
   call printf
 
-  lea rdi, [format_str]
-  lea rsi, [print_newl] 
-  mov rax, 0 
-  call printf
-
-  pop rcx 
-  pop r8 
-  pop r9
-  pop r10
-
-  add rsp, 8
-
-  jmp checkEnd
-
-handleEnd:
-
-  ; load size of r13-15
-  mov rbx, [r12 + 180]
-  imul rbx, 8
-  imul rbx, 3
-
-  ; unmap memory
-  
-  ; array memory
-  mov rdi, [r12 + 196]
-  mov rsi, rbx
-  syscall
-
   ; close files
-  mov rax, 3
+  mov rax, 3 
   mov rdi, [r12]
   syscall
-
-  mov rdi, [r12 + 256]
-  syscall
-
-  ; aggregate heap
-  mov rax, 11
-  mov rdi, r12
-  mov rsi, 512
+  mov rdi, [r12 + 8]
   syscall 
 
+  ; free clean and outlier arrays
+  mov rbx, [r12 + 180]
+  imul rbx, 8
+  imul rbx, 2 
 
-  ; Handle Exit Gracefully
-  xor rdi, rdi                     ; set return code
-  mov rax, 60                      ; sys_exit call
+  mov rax, 11 
+  mov rdi, r14
+  mov rsi, rbx 
+  syscall 
+
+  ; free agg heap
+  mov rax, 11 
+  mov rdi, r12 
+  mov rsi, 512 
+  syscall 
+
+  xor rdi, rdi
+  mov rax, 60
   syscall
 
-error:
-  ; Exit Due to Error
-  mov rdi, 1                       ; set return code
-  mov rax, 60                      ; sys_exit call
+error: 
+
+  ; close files
+  mov rax, 3 
+  mov rdi, [r12]
   syscall
+  mov rdi, [r12 + 8]
+  syscall 
+
+  ; free agg heap
+  mov rax, 11 
+  mov rdi, r12 
+  mov rsi, 512 
+  syscall 
+
+  xor rdi, rdi
+  mov rax, 60
+  syscall
+
+; Aggregates M2 and mean 
+;
+; Registers used: 
+;   rdi:  current count
+;   rsi:  pointer to mean
+;   rdx:  pointer to M2
+;   xmm0: current value
+;
+_sigma: 
+  
+  ; pull mean and M2 from memory
+  movsd xmm1, [rsi]
+  movsd xmm2, [rdx]
+
+  ; count to double
+  cvtsi2sd xmm3, rdi
+
+  ; subtract mean from current value and store in xmm4
+  movsd xmm4, xmm0
+  subsd xmm4, xmm1
+
+  ; update mean with delta 
+  divsd xmm4, xmm3 
+  addsd xmm1, xmm4
+
+  ; subtract new mean from valule and store in xmm5
+  movsd xmm5, xmm0
+  subsd xmm5, xmm1 
+
+  ; add to M2 with delta * delta2
+  mulsd xmm4, xmm5 
+  addsd xmm2, xmm4
+
+  ; place new values in memory
+  movsd [rsi], xmm1
+  movsd [rdx], xmm2 
+
+  ret
+
+; 
+;  rdi: threshold
+;  rsi: iter's sigma 
+;
+;  ret xmm0 holds threshold
+;
+_threshold:
+  
+  ; calculate threshold
+  movsd xmm0, [r12 + 16]
+  mulsd xmm0, [r12 + 64]
+
+  ret
